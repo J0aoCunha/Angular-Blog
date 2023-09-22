deb0b54e84d787bd576683e2299d92040f7e7f6d
joaocunha <jvscunha04@gmail.com>
2023-09-22 00:32:50 -0300

    feat: create menu-title

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuBarComponent } from './compoents/menu-bar/menu-bar.component';
+import { MenuTitleComponent } from './compoents/menu-title/menu-title.component';
+import { BigCardComponent } from './compoents/big-card/big-card.component';
+import { SmallCArdComponent } from './compoents/small-card/small-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuBarComponent,
+    MenuTitleComponent,
+    BigCardComponent,
+    SmallCArdComponent
   ],
   imports: [
     BrowserModule,
