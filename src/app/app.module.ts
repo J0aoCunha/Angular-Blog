5274a1de91b693a19b23d12d88b5bf1d6a6081db
joaocunha <jvscunha04@gmail.com>
2023-09-23 13:09:11 -0300

    feat: create the home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { MenuBarComponent } from './compoents/menu-bar/menu-bar.component';
 import { MenuTitleComponent } from './compoents/menu-title/menu-title.component';
 import { BigCardComponent } from './compoents/big-card/big-card.component';
 import { SmallCArdComponent } from './compoents/small-card/small-card.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { SmallCArdComponent } from './compoents/small-card/small-card.component'
     MenuBarComponent,
     MenuTitleComponent,
     BigCardComponent,
-    SmallCArdComponent
+    SmallCArdComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
