290fc54ba163cff5cd663bb3d8373b8abda8ab37
joaocunha <jvscunha04@gmail.com>
2023-09-25 23:00:09 -0300

    feat: creating a fake data base

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+    {
+        "id": 1,
+        "photo": "aaa",
+        "title": "aaaaaaaaaa",
+        "description": "ola mundo"
+    },
+    {
+        "id": 2,
+        "photo": "aaa",
+        "title": "aaaaaaaaaa",
+        "description": "ola mundo"
+    }
+]
